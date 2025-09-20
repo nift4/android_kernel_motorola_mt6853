53ab2801cd35664d7f7ccb718a963ff40bd947ad
bengris32 <bengris32@protonmail.ch>
2025-09-20 01:26:17 +0100

    Revert "[ALPS04983389] spf: permit it to go spf when swapping in a single page"
    This reverts commit 1294f73667779ebd73ef4fc1e5fc3b9664d9a686.
    
    Change-Id: I01c8227aeffdcb45e0e70b6f6765b5a69aa01d08
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mm/memory.c
+++ mm/memory.c
@@ -3378,8 +3378,7 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 				lru_cache_add_anon(page);
 				swap_readpage(page, true);
 			}
-		} else if (swap_use_vma_readmore() &&
-				vmf->flags & FAULT_FLAG_SPECULATIVE) {
+		} else if (vmf->flags & FAULT_FLAG_SPECULATIVE) {
 			/*
 			 * Don't try readahead during a speculative page fault
 			 * as the VMA's boundaries may change in our back.
